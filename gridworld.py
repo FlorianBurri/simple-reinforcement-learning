0836dd40e735185fe6d5edc2eadd6c31c5fc21e0
FlorianBurri <f6.burri@gmail.com>
2019-06-19 22:09:18 +0200

    create enviroment class

--- gridworld.py
+++ gridworld.py
@@ -6,108 +6,113 @@ RIGHT = 2
 LEFT = 3
 GOAL = [3,7]
 
-def render(state):
-    ''' Renders current Gridworld
-    INPUT: state: current location: [row, column]
-    T is the goal / terminal state
-    X is the current position
-    '''
 
-    # make sure input is valid    
-    if (0 > state[0] > 6) or (0 > state[1] > 9):
-        print("Error! state out of range: ", state)
-        return -1
-    
-    if state == GOAL:
-        print("SUCCESS!!!")
-    
-    for row in range(7):
-        print()
-        for col in range(10):
-            if [row, col] == state:
-                print(" X", end='')
-            elif [row, col] == GOAL:
-                print(" G", end='')
-            else:
-                print(" *", end='')
-    print("\n ___________________")
-    print(" 0 0 0 1 1 1 2 2 1 0")
+class environment:
+    def __init__(self):
+        self.state = [3, 0]
+        self.path = [self.state]
 
-
-def renderPath(path):
-    ''' Renders a path in the gridworld
-    INPUT: list of states
-    T is the goal / terminal state
-    X is the path
-    '''
     
-    for row in range(7):
-        print()
-        for col in range(10):
-            if [row, col] == GOAL:
-                print(" G", end='')
-            elif [row,col] in path:
-                print(" X", end='')
-            else:
-                print(" *", end='')
-    print("\n ___________________")
-    print(" 0 0 0 1 1 1 2 2 1 0")
-
-
-def env(state, action, render_s=False):
-    ''' Returns next state and reward after taking action
-    Inputs:
-        state: current location: [row, column]
-        action: int 0 <= action <= 3
-        render: bool
-    Output:
-        nextState: new location: [row, column]
-        reward: int (always -1 unless in goal state, then 0)
-        goalReached: bool
-    '''
-    # make sure input is valid    
-    if (0 > state[0] > 6) or (0 > state[1] > 9):
-        print("Error! state out of range: ", state)
-        return -1
-    
-    if action not in [0,1,2,3]:
-        print("Invalid action:", action)
-        return -1
-    
-    if state == GOAL:
-        return state, 0, True
+    def reset(self):
+        self.state = [3, 0]
+        self.path = [self.state]
         
-    nextState = state.copy()
     
-    # take action
-    if action == UP and state[0] > 0:
-        nextState[0] -= 1
-    elif action == DOWN and state[0] < 6:
-        nextState[0] += 1
-    elif action == RIGHT and state[1] < 9:
-        nextState[1] += 1
-    elif action == LEFT and state[1] > 0:
-        nextState[1] -= 1
+    def step(self, action):
+        ''' Returns next state and reward after taking action
+        Inputs:
+            action: int 0 <= action <= 3
+        Output:
+            nextState: new location: [row, column]
+            reward: int (always -1 unless in goal state, then 0)
+            goalReached: bool
+        '''
+        # make sure input is valid            
+        if action not in [0,1,2,3]:
+            print("Invalid action:", action)
+            return -1
         
-    # add wind
-    wind = [0,0,0,1,1,1,2,2,1,0]
-    current_wind = wind[state[1]]
-    nextState[0] -= current_wind
-    if nextState[0] < 0:
-        nextState[0] = 0
-    
-    if nextState == GOAL:
-        goalReached = True
-        reward = 0
-    else:
-        goalReached = False
-        reward = -1
-    
-    if render_s == True:
-        render(state)
-        render(nextState)
+        if self.state == GOAL:
+            return self.state, 0, True
+            
+        nextState = self.state.copy()
         
-    return nextState, reward, goalReached
+        # take action
+        if action == UP and self.state[0] > 0:
+            nextState[0] -= 1
+        
+        elif action == DOWN and self.state[0] < 6:
+            nextState[0] += 1
+        
+        elif action == RIGHT and self.state[1] < 9:
+            nextState[1] += 1
+        
+        elif action == LEFT and self.state[1] > 0:
+            nextState[1] -= 1
+            
+        # add wind
+        wind = [0,0,0,1,1,1,2,2,1,0]
+        current_wind = wind[self.state[1]]
+        nextState[0] -= current_wind
+        if nextState[0] < 0:
+            nextState[0] = 0
+        
+        if nextState == GOAL:
+            goalReached = True
+            reward = 0
+        else:
+            goalReached = False
+            reward = -1
+            
+        self.state = nextState
+        self.path.append(nextState)
+        return self.state, reward, goalReached
+          
+    
+    def render(self):
+        ''' Renders current Gridworld state
+        T is the goal / terminal state
+        X is the current position
+        '''
+    
+        # make sure input is valid    
+        if (0 > self.state[0] > 6) or (0 > self.state[1] > 9):
+            print("Error! state out of range: ", self.state)
+            return -1
+        
+        if self.state == GOAL:
+            print("SUCCESS!!!")
+        
+        for row in range(7):
+            print()
+            for col in range(10):
+                if [row, col] == self.state:
+                    print(" X", end='')
+                elif [row, col] == GOAL:
+                    print(" G", end='')
+                else:
+                    print(" *", end='')
+        print("\n ___________________")
+        print(" 0 0 0 1 1 1 2 2 1 0")
+
+
+    def renderPath(self):
+        ''' Renders a path in the gridworld
+        T is the goal / terminal state
+        X is the path
+        '''        
+        for row in range(7):
+            print()
+            for col in range(10):
+                if [row, col] == GOAL:
+                    print(" G", end='')
+                elif [row,col] in self.path:
+                    print(" X", end='')
+                else:
+                    print(" *", end='')
+        print("\n ___________________")
+        print(" 0 0 0 1 1 1 2 2 1 0")
+
 
 
 def eGreedy(actionValues, epsilon):
@@ -128,8 +133,10 @@ def eGreedy(actionValues, epsilon):
     
     return action
 
+
 def dynamicProgramming(maxDelta=0):
     V = np.zeros((7,10))
+    env = environment()
     while True:
         delta = 0
         # loop over every state
@@ -139,9 +146,13 @@ def dynamicProgramming(maxDelta=0):
                 next_values = []
                 # try every action in this state
                 for action in range(4):
-                    nextState, reward, _ = env([row,col], action)
+                    # for dynamicPogramming we need to be able to change
+                    # the state of the environment manualy:
+                    env.state = [row,col]
+                    nextState, reward, _ = env.step(action)
                     nextValue = V[nextState[0], nextState[1]] + reward
                     next_values.append(nextValue)
+                    
                 V[row,col] = max(next_values)
                 delta = max(delta, abs(value-V[row,col]))
         
